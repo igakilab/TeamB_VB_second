cd50b1cbbbcccf8eb3b5d872da2f1f3782419ec4
wuyou284 <wuyou284@gmail.com>
2018-02-02 11:06:49 +0900

    taskC-3-1
    タスクC-3-1 商品グループのフィルタ機能を行った。

--- frmShohin.vb
+++ frmShohin.vb
@@ -357,4 +357,21 @@ Public Class frmShohin
         '保存
         m_fm.odaShohin.Update(DsSample1, "T_商品")
     End Sub
+
+    Private Sub mnuRecordFilter_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mnuRecordFilter.Click
+        Dim fm As New frmDialog()   '検索フォーム
+
+        'キャンセルされたとき
+        If fm.ShowDialog = DialogResult.Cancel Then
+            Exit Sub
+        End If
+
+        '値が入力されなかったとき
+        If fm.Value = "" Then
+            Exit Sub
+        End If
+
+        'フィルタ実行
+        dvShohin.RowFilter = "商品グループ = '" & fm.Value & "'"
+    End Sub
 End Class
